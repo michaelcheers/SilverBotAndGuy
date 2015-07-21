8c4bb0310c14dc1b3445f451260ad6fd7f10754d
Michael Cheers <michaelcheers8@gmail.com>
2015-07-21 11:22:46 +0100

    First real commit
    Added game

---
+++ SilverBotAndGuy/SilverBotAndGuy/MainGame.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilverBotAndGuy
+{
+    class MainGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        public MainGame () : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        protected override void OnExiting(object sender, EventArgs args)
+        {
+            Process.GetCurrentProcess().Kill();
+            base.OnExiting(sender, args);
+        }
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            base.LoadContent();
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ SilverBotAndGuy/SilverBotAndGuy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilverBotAndGuy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainGame game = new MainGame();
+            game.Run();
+        }
+    }
+}

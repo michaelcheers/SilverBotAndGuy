17fcd8f17279f39445408774f23baf58457adf01
LaurieCheers <lauriecheers@gmail.com>
2015-07-23 20:31:45 +0100

    Graphics tests

--- SilverBotAndGuy/SilverBotAndGuy/MainGame.cs
+++ SilverBotAndGuy/SilverBotAndGuy/MainGame.cs
@@ -1,5 +1,6 @@
 ﻿using Input;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using SilverBotAndGuy.Graphics;
@@ -33,20 +34,41 @@ namespace SilverBotAndGuy
     }
     class MainGame : Game
     {
-        Texture2D4D laserGun;
-        Texture2D4D dozerBot;
-        Texture2D4D silverBot;
-        Texture2D laserRight;
-        Texture2D laserDown;
-        Texture2D laserPulse;
-        Texture2D crate;
-        Texture2D bomb;
-        Texture2D floor;
+        class GameTextures
+        {
+            public Texture2D4D laserGun;
+            public Texture2D4D dozerBot;
+            public Texture2D4D silverBot;
+            public Texture2D laserRight;
+            public Texture2D laserDown;
+            public Texture2D laserPulse;
+            public Texture2D crate;
+            public Texture2D bomb;
+            public Texture2D floor;
+            public Texture2D wall;
+            public Texture2D panel;
+
+            public GameTextures(ContentManager Content)
+            {
+                floor = Content.Load<Texture2D>("floor");
+                wall = Content.Load<Texture2D>("wall");
+                crate = Content.Load<Texture2D>("crate");
+                panel = Content.Load<Texture2D>("panel");
+                bomb = Content.Load<Texture2D>("bomb");
+                laserRight = Content.Load<Texture2D>("laser-right");
+                laserDown = Content.Load<Texture2D>("laser-down");
+                laserPulse = Content.Load<Texture2D>("laser-pulse");
+                laserGun = new Texture2D4D(Content, "lasergun");
+                dozerBot = new Texture2D4D(Content, "dozerbot");
+                silverBot = new Texture2D4D(Content, "mirrorbot");
+            }
+        }
         Block[,] blocks;
         InputState inputState = new InputState();
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         LaserbeamManager laserManager;
+        GameTextures textures;
 
         public static Random rand = new Random();
 
@@ -75,17 +97,8 @@ namespace SilverBotAndGuy
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            floor = Content.Load<Texture2D>("floor");
-            crate = Content.Load<Texture2D>("crate");
-            bomb = Content.Load<Texture2D>("bomb");
-            laserRight = Content.Load<Texture2D>("laser-right");
-            laserDown = Content.Load<Texture2D>("laser-down");
-            laserPulse = Content.Load<Texture2D>("laser-pulse");
-         //   laserGun = new Texture2D4D(Content, "lasergun");
-            dozerBot = new Texture2D4D(Content, "dozerbot");
-            silverBot = new Texture2D4D(Content, "mirrorbot");
-
             laserManager = new LaserbeamManager(Content);
+            textures = new GameTextures(Content);
 
             base.LoadContent();
         }
@@ -93,8 +106,8 @@ namespace SilverBotAndGuy
         {
             return new Rectangle(x * widthOfBlock, y * heightOfBlock, widthOfBlock, heightOfBlock);
         }
-        const int widthOfBlock = 64;
-        const int heightOfBlock = 64;
+        const int widthOfBlock = 32;
+        const int heightOfBlock = 32;
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
@@ -106,20 +119,35 @@ namespace SilverBotAndGuy
                     Block current = blocks[x, y];
                     if (current.HasFlag(Block.Crate))
                     {
-                        spriteBatch.Draw(crate, GetPosition(x, y), Color.White);
+                        spriteBatch.Draw(textures.crate, GetPosition(x, y), Color.White);
                     }
                     else if (current.HasFlag(Block.Bomb))
                     {
-                        spriteBatch.Draw(bomb, GetPosition(x, y), Color.White);
+                        spriteBatch.Draw(textures.bomb, GetPosition(x, y), Color.White);
                     }
                     else
                     {
-                        spriteBatch.Draw(floor, GetPosition(x, y), Color.White);
+                        spriteBatch.Draw(textures.floor, GetPosition(x, y), Color.White);
                     }
                 }
             }
 
-            spriteBatch.Draw(dozerBot.Get(botDirection), GetPosition(0, 0), Color.White);
+            for (int x = 1; x < 10; x++ )
+            {
+                for (int y = 8; y < 14; y++ )
+                {
+                    spriteBatch.Draw(textures.floor, GetPosition(x, y), Color.White);
+                }
+            }
+            spriteBatch.Draw(textures.wall, GetPosition(8, 11), Color.White);
+            spriteBatch.Draw(textures.wall, GetPosition(8, 12), Color.White);
+            spriteBatch.Draw(textures.panel, GetPosition(4, 12), Color.White);
+            spriteBatch.Draw(textures.crate, GetPosition(2, 12), Color.White);
+
+            spriteBatch.Draw(textures.laserGun.Get(Direction4D.Right), GetPosition(2, 10), Color.White);
+            spriteBatch.Draw(textures.dozerBot.Get(botDirection), GetPosition(0, 0), Color.White);
+            Rectangle silverBotPos = GetPosition(5, 9);
+            spriteBatch.Draw(textures.silverBot.Get(botDirection), new Vector2(silverBotPos.X, silverBotPos.Y), Color.White);
 
             laserManager.Draw(spriteBatch);
             spriteBatch.End();

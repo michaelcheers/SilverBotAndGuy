5abe8bc8f67ad11b8a63de1ea19ca2094b47afcc
Michael Cheers <michaelcheers8@gmail.com>
2015-07-24 08:23:45 +0100

    Put GameTextures in it's own file.

--- SilverBotAndGuy/SilverBotAndGuy/Graphics/MainGame.cs
+++ SilverBotAndGuy/SilverBotAndGuy/Graphics/MainGame.cs
@@ -23,37 +23,8 @@ namespace SilverBotAndGuy
         Right = -2,
     }
 
-    class MainGame : Game
+    partial class MainGame : Game
     {
-        class GameTextures
-        {
-            public Texture2D4D laserGun;
-            public Texture2D4D dozerBot;
-            public Texture2D4D silverBot;
-            public Texture2D laserRight;
-            public Texture2D laserDown;
-            public Texture2D laserPulse;
-            public Texture2D crate;
-            public Texture2D bomb;
-            public Texture2D floor;
-            public Texture2D wall;
-            public Texture2D panel;
-
-            public GameTextures(ContentManager Content)
-            {
-                floor = Content.Load<Texture2D>("floor");
-                wall = Content.Load<Texture2D>("wall");
-                crate = Content.Load<Texture2D>("crate");
-                panel = Content.Load<Texture2D>("panel");
-                bomb = Content.Load<Texture2D>("bomb");
-                laserRight = Content.Load<Texture2D>("laser-right");
-                laserDown = Content.Load<Texture2D>("laser-down");
-                laserPulse = Content.Load<Texture2D>("laser-pulse");
-                laserGun = new Texture2D4D(Content, "lasergun");
-                dozerBot = new Texture2D4D(Content, "dozerbot");
-                silverBot = new Texture2D4D(Content, "mirrorbot");
-            }
-        }
         Block[,] blocks;
         InputState inputState = new InputState();
         GraphicsDeviceManager graphics;
